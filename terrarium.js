789e2141deda0b001493b312542d642557968d6a
mala23 <mail@mala23.me>
2014-11-30 16:20:20 +0000

    defining terrarium plan and adding points in space

---
+++ terrarium.js
@@ -0,0 +1,21 @@
+var plan = ["############################",
+            "#      #    #      o      ##",
+            "#                          #",
+            "#          #####           #",
+            "##         #   #    ##     #",
+            "###           ##     #     #",
+            "#           ###      #     #",
+            "#   ####                   #",
+            "#   ##       o             #",
+            "# o  #         o       ### #",
+            "#    #                     #",
+            "############################"];
+
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Point.prototype.add = function(other) {
+  return new Point (this.x + other.x, this.y + other.y);
+};
